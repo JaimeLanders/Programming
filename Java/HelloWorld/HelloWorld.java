9bb8b221e49836be1008ef652d87583e326e0d1f
Jaime Landers <jaimelanders@gmail.com>
2020-10-15 17:40:00 -0700

    Update remote

--- Java/HelloWorld/HelloWorld.java
+++ Java/HelloWorld/HelloWorld.java
@@ -1,10 +1,10 @@
-
 /**
  * Write a description of class HelloWorld here.
  *
  * @author (your name)
  * @version (a version number or a date)
  */
+
 public class HelloWorld
 {
     // instance variables - replace the example below with your own
